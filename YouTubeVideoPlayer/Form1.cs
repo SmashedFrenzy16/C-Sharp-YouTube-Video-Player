1755bf13638801a74f474fc8b94aae06a727c3ab
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-10-03 12:43:04 +0100

    Update Form1.cs with code

--- YouTubeVideoPlayer/Form1.cs
+++ YouTubeVideoPlayer/Form1.cs
@@ -1,1 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YouTubeVideoPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            youtubePlayer.Navigate(textBox.Text);
+        }
+    }
+}
 
