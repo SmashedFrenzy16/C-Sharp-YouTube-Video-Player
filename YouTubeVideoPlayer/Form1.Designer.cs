d517c0fc7cc41fdaf6db7dd12da7ddd9568285aa
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-10-03 12:44:53 +0100

    Update Form1.Designer.cs with code

--- YouTubeVideoPlayer/Form1.Designer.cs
+++ YouTubeVideoPlayer/Form1.Designer.cs
@@ -1,1 +1,84 @@
+namespace YouTubeVideoPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.youtubePlayer = new System.Windows.Forms.WebBrowser();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // youtubePlayer
+            // 
+            this.youtubePlayer.Location = new System.Drawing.Point(3, 103);
+            this.youtubePlayer.MinimumSize = new System.Drawing.Size(20, 20);
+            this.youtubePlayer.Name = "youtubePlayer";
+            this.youtubePlayer.Size = new System.Drawing.Size(1885, 860);
+            this.youtubePlayer.TabIndex = 0;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(13, 13);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(1056, 31);
+            this.textBox.TabIndex = 1;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(1087, 13);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(104, 46);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1889, 968);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.youtubePlayer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser youtubePlayer;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button submitButton;
+    }
+}
+
 
